cc3ab2b56b26bb45154942e74b47a418def72ad5
houce <houce@baidu.com>
2017-03-23 15:27:14 +0800

    add

--- build/main.js
+++ build/main.js
@@ -59,7 +59,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "63ede2bccebc84cb360a"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "5fa04594b8d6810942f0"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotMainModule = true; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
